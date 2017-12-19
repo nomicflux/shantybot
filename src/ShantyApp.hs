391cd3a279c15f871700bde86b6f9d3ee044b8e4
Michael A <nomicflux@gmail.com>
2017-12-19 07:18:12 -0600

    Tweaked message formatting

--- src/ShantyApp.hs
+++ src/ShantyApp.hs
@@ -11,7 +11,9 @@ import Control.Monad ((<=<))
 import Control.Monad.Loops (iterateM_)
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Trans.Control (MonadBaseControl)
-import Data.Maybe (mapMaybe)
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Maybe (mapMaybe, fromMaybe)
 import Data.Monoid ((<>))
 import Data.Text (Text)
 import Data.Text.IO (readFile, writeFile)
@@ -41,26 +43,34 @@ unPascalCase text = go (breaker text) ""
     go (n, "") res = T.strip $ res <> n
     go (n, m) res = go (breaker $ T.drop 1 m) (res <> n <> " " <> T.take 1 m)
 
-respondToTweet :: TfIdf -> Tweet -> Maybe (Tweet, Text)
-respondToTweet tfidf tweet =
-  (\s -> (tweet, respondToPerson <> greeting <> s <> "?")) <$> song
+wordsToLimit :: Int -> Text -> Text
+wordsToLimit n text = go (T.strip <$> T.words text) "" 0
   where
-    respondToPerson :: Text
-    respondToPerson = "@" <> (tweetUserScreenName $ tweetUser tweet)  <> " "
-    greeting :: Text
-    greeting = "Ahoy " <> (tweetUserName $ tweetUser tweet) <> ", perhaps you want: "
-    song :: Maybe Text
-    song = unPascalCase <$> matchPhrase (filterOutMention $ tweetText tweet) tfidf
+    go [] res _ = T.strip res
+    go (x:xs) res m =
+      if m + T.length x > n then res else go xs (res <> " " <> x) (m + T.length x + 1)
 
-runCycle :: MonadIO m => Manager -> TfIdf ->
+respondToTweet :: Map Text Text -> TfIdf -> Tweet -> Maybe (Tweet, Text)
+respondToTweet docMap tfidf tweet = songMsg <$> song
+  where
+    respondToPerson = "@" <> (tweetUserScreenName $ tweetUser tweet)  <> " "
+    name = T.unwords . take 1 . T.words . tweetUserName . tweetUser $ tweet
+    greeting = "Ahoy " <> name <> ", perhaps you want: "
+    song = matchPhrase (filterOutMention $ tweetText tweet) tfidf
+    songMsg s = let msg = greeting <> unPascalCase s <> "? "
+                    size = T.length msg
+                    songText = fromMaybe "" $ M.lookup s docMap
+                in (tweet, respondToPerson <> msg <> wordsToLimit (140 - size - 4) songText <> " ...")
+
+runCycle :: MonadIO m => Manager -> Map Text Text -> TfIdf ->
   (Maybe Text -> m Request) ->
   (Tweet -> Text -> m Request) ->
   Maybe Text -> m (Maybe Text)
-runCycle manager tfidf get post prevId = do
+runCycle manager docMap tfidf get post prevId = do
   getter <- get prevId
   (tweets :: [Tweet]) <- queryEndpoint manager getter
   liftIO $ L.log' . T.pack $ show tweets
-  let responses = mapMaybe (respondToTweet tfidf) tweets
+  let responses = mapMaybe (respondToTweet docMap tfidf) tweets
   liftIO $ L.debug' . T.pack $ show responses
   mapM_ (useEndpoint manager <=< uncurry post) responses
   let
@@ -78,13 +88,13 @@ runService = do
     logger (Just logfile) = L.withFileLogging logfile
   let mainProg = do
         manager <- newManager tlsManagerSettings
-        tfidf <- getTfIdfFromDir songDirectory 1 4
+        (docs, tfidf) <- getTfIdfFromDir songDirectory 1 4
         let
           idFile = "data/lastId"
           returnId prevId Nothing  = prevId
           returnId _ i@(Just _) = i
           fromId prevId = do
-            nextId <- runCycle manager tfidf (getMentions cfg) (postReply cfg) prevId
+            nextId <- runCycle manager docs tfidf (getMentions cfg) (postReply cfg) prevId
             maybe (return ()) (writeFile idFile) nextId
             threadDelay (1000 * 1000 * (configPollFreq cfg))
             return $ returnId prevId nextId
--- src/TextMining/RetrievalService.hs
+++ src/TextMining/RetrievalService.hs
@@ -4,8 +4,9 @@ module TextMining.RetrievalService where
 import Prelude hiding (readFile)
 
 import System.Directory
+import qualified Data.Map as M
 import Data.Monoid ((<>))
-import Data.Text (pack)
+import Data.Text (pack, Text)
 import Data.Text.IO (readFile)
 
 import TextMining.Document
@@ -13,17 +14,20 @@ import TextMining.Document
 songDirectory :: FilePath
 songDirectory = "data/songs"
 
-fileToDoc :: FilePath -> FilePath -> IO Document
+fileToDoc :: FilePath -> FilePath -> IO (Text, Document)
 fileToDoc dir fileName =
   let docName = pack $ takeWhile (/= '.') fileName
       filePath = dir <> "/" <> fileName
-  in (mkDocument docName) <$> readFile filePath
+  in (\t -> (t, mkDocument docName t)) <$> readFile filePath
 
-getFiles :: FilePath -> IO [Document]
+getFiles :: FilePath -> IO [(Text, Document)]
 getFiles dir = do
   files <- listDirectory dir
   mapM (fileToDoc dir) files
 
-getTfIdfFromDir :: FilePath -> Int -> Int -> IO TfIdf
-getTfIdfFromDir dir minGrams maxGrams =
-  genTfIdfFromDocs minGrams maxGrams <$> getFiles dir
+getTfIdfFromDir :: FilePath -> Int -> Int -> IO (M.Map Text Text, TfIdf)
+getTfIdfFromDir dir minGrams maxGrams = do
+  docs <- getFiles dir
+  let docMap = M.fromList $ (\(t, d) -> (docName d, t)) <$> docs
+      tfidf = genTfIdfFromDocs minGrams maxGrams (snd <$> docs)
+  return (docMap, tfidf)
